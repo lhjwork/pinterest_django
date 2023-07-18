1233a07677a4087c4294db8145eba924a99f529c
lhjwork <jin.come.up.business@gmail.com>
2023-07-18 23:40:56 +0900

    30 course studing as making profileapp

--- accountapp/forms.py
+++ accountapp/forms.py
@@ -4,5 +4,6 @@ class AccountUpdateForm(UserCreationForm):
     def __intf__(self, *args, **kwargs):
         super().__init__(*args,**kwargs)
 
+        # id 값에 해당함
         self.fields['username'].disabled = True
 
