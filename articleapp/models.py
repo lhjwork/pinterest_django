c4879b55d001f0072a77af7b597ba57d95793a5a
lhjwork <jin.come.up.business@gmail.com>
2023-07-25 15:13:27 +0900

    35. Article Implementation working class ArticleCreateView(CreateView):

--- articleapp/models.py
+++ articleapp/models.py
@@ -1,3 +1,14 @@
+from django.contrib.auth.models import User
 from django.db import models
 
 # Create your models here.
+class Article(models.Model):
+    # set_null article이 회원 탈퇴했을떄 article이 사라지지 않고 알수 없는 게시글로 남도록하기 위해서
+    # related_name='article' -> user 객체에서 article에 접근할떄 쓰는 네임이어서 더 직관적이므로
+    writer = models.ForeignKey(User, on_delete=models.SET_NULL, related_name='article', null=True)
+
+    title = models.CharField(max_length=200, null=True)
+    image = models.ImageField(upload_to='article/', null=False)
+    content = models.TextField(null=True)
+
+    create_at = models.DateField(auto_created=True, null=True)
--- articleapp/views.py
+++ articleapp/views.py
@@ -1,3 +1,22 @@
 from django.shortcuts import render
+from django.urls import reverse
+from django.views.generic import CreateView
+
+from articleapp.forms import ArticleCreationForm
+from articleapp.models import Article
+
 
 # Create your views here.
+class ArticleCreateView(CreateView):
+    model = Article
+    from_class = ArticleCreationForm
+    template_name = 'articleapp/create.html'
+
+    def form_valid(self, form):
+        temp_article = form.save(commit=False)
+        temp_article.writer = self.request.user
+        temp_article.save()
+        return super().form_valid(form)
+
+    def get_success_url(self):
+        return reverse('articleapp:detail', kwargs={'pk' : self.object.pk})
