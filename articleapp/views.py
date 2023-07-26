dc0eaedfb3a18a7ae990d4ad2fd09cc079b9841b
lhjwork <jin.come.up.business@gmail.com>
2023-07-26 14:45:38 +0900

    35.ArticleUpdateView end

--- articleapp/views.py
+++ articleapp/views.py
@@ -1,12 +1,17 @@
+from django.contrib.auth.decorators import login_required
 from django.shortcuts import render
 from django.urls import reverse
-from django.views.generic import CreateView, DetailView
+from django.utils.decorators import method_decorator
+from django.views.generic import CreateView, DetailView, UpdateView
 
+from articleapp.decorators import article_ownership_required
 from articleapp.forms import ArticleCreationForm
 from articleapp.models import Article
 
 
 # Create your views here.
+@method_decorator(login_required,'get')
+@method_decorator(login_required,'post')
 class ArticleCreateView(CreateView):
     model = Article
     form_class = ArticleCreationForm
@@ -25,3 +30,15 @@ class ArticleDetailView(DetailView):
         model = Article
         context_object_name = 'target_article'
         template_name = 'articleapp/detail.html'
+
+
+@method_decorator(article_ownership_required,'get')
+@method_decorator(article_ownership_required,'post')
+class ArticleUpdateView(UpdateView):
+    model = Article
+    context_object_name = 'target_article'
+    form_class = ArticleCreationForm
+    template_name = 'articleapp/update.html'
+
+    def get_success_url(self):
+        return reverse('articleapp:detail', kwargs={'pk': self.object.pk})
