5a8e6da246b074c912f849e950111605cf8cddaf
lhjwork <jin.come.up.business@gmail.com>
2023-07-30 18:36:42 +0900

    40 course migrate

--- projectapp/models.py
+++ projectapp/models.py
@@ -3,7 +3,7 @@ from django.db import models
 # Create your models here.
 
 class Project(models.Model):
-    image = models.ImageField(update_to='project/', null=False)
+    image = models.ImageField(upload_to='project/', null=False)
     title = models.CharField(max_length=20, null=False)
     description = models.CharField(max_length=200, null=False)
 
