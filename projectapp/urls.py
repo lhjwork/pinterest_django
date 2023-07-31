16f7e39daff79496e33e61e6d5c92965ee1ce40c
lhjwork <jin.come.up.business@gmail.com>
2023-07-31 11:29:42 +0900

    40. Projectapp Implementation create,detail, list View end by 10:30

--- projectapp/urls.py
+++ projectapp/urls.py
@@ -1,14 +1,16 @@
 from django.urls import path
 
-from projectapp.views import ProjectCreateView
+from projectapp.views import ProjectCreateView, ProjectDetailView, ProjectListView
 
 app_name= 'projectapp'
 
 urlpatterns = [
     # path('create/', ProfileCreateView.as_view(), name='create'),
     # path('update/<int:pk>', ProfileUpdateView.as_view(), name='update'),
-
+    path('list/', ProjectListView.as_view(), name='list'),
 
     path('create/',ProjectCreateView.as_view(), name='create'),
+    path('detail/<int:pk>', ProjectDetailView.as_view(), name='detail'),
+
 
 ]
--- projectapp/views.py
+++ projectapp/views.py
@@ -2,7 +2,7 @@ from django.contrib.auth.decorators import login_required
 from django.shortcuts import render
 from django.urls import reverse
 from django.utils.decorators import method_decorator
-from django.views.generic import CreateView
+from django.views.generic import CreateView, DetailView, ListView
 
 from projectapp.forms import ProjectCreationForm
 from projectapp.models import Project
@@ -18,5 +18,17 @@ class ProjectCreateView(CreateView):
     template_name = 'projectapp/create.html'
 
     def get_success_url(self):
-        return reverse('project:detail', kwargs={'pk': self.object.pk})
+        return reverse('projectapp:detail', kwargs={'pk': self.object.pk})
 
+
+class ProjectDetailView(DetailView):
+    model = Project
+    context_object_name = 'target_project'
+    template_name = 'projectapp/detail.html'
+
+
+class ProjectListView(ListView):
+    model = Project
+    context_object_name = 'project_list'
+    template_name = 'projectapp/list.html'
+    paginate_by = 25
