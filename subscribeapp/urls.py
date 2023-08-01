15945d307401eb22720a431039999be83c7fda1d
lhjwork <jin.come.up.business@gmail.com>
2023-08-01 18:33:53 +0900

    43. Subscription page implementation with Field Lookup end

--- subscribeapp/urls.py
+++ subscribeapp/urls.py
@@ -1,12 +1,13 @@
 from django.urls import path
 
 from profileapp.views import ProfileCreateView, ProfileUpdateView
-from subscribeapp.views import SubscriptionView
+from subscribeapp.views import SubscriptionView, SubscriptionListView
 
 app_name= 'subscribeapp'
 
 urlpatterns = [
 
     path('subscribe/', SubscriptionView.as_view(), name='subscribe'),
+    path('list/', SubscriptionListView.as_view(), name='list'),
 
 ]
