9788b8e4dc6828bde5db3d2fc5a998a4a72e5d12
lhjwork <jin.come.up.business@gmail.com>
2023-08-15 20:57:50 +0900

    course 45 studing material-icons applied and not need code is adjustments

--- accountapp/urls.py
+++ accountapp/urls.py
@@ -1,14 +1,13 @@
 from django.contrib.auth.views import LoginView, LogoutView
 from django.urls import path
 
-from accountapp.views import hello_world, AccountCreateView, AccountDetailView, AccountUpdateView, AccountDeleteView
+from accountapp.views import AccountCreateView, AccountDetailView, AccountUpdateView, AccountDeleteView
 
 app_name="accountapp"
 
 # "127.0.0.1:8000/account/hello_world.html"
 # 나중에는 저기 name으로 accountapp:hello_world.html 라는 식으로 간편하게 하는 함수가 있단다.
 urlpatterns = [
-    path('hello_world/', hello_world,  name='hello_world'),
 
     path('login/', LoginView.as_view(template_name='accountapp/login.html'), name='login'),
     path('logout/', LogoutView.as_view(), name='logout'),
